07d24ede1858327141cadb95abc26c54f88f4ba8
mjeightyfive <mjeightyfive@gmail.com>
2019-05-10 15:11:05 +0100

    add key spacing test

--- tests/config.test.js
+++ tests/config.test.js
@@ -6,12 +6,17 @@ const cli = new CLIEngine({
     configFile: '.eslintrc.json'
 });
 
-test('load config in eslint to validate all rule syntax is correct', () => {
+test('load config in eslint to validate rule syntax is correct', () => {
     const code = 'const foo = 1;\nconst bar = function() {};\nbar(foo);\n(async () => {})();\n';
     expect(cli.executeOnText(code).errorCount).toBe(0);
 });
 
 test('import syntax is correct', () => {
     const code = `import { foo } from 'foo';\n`;
+    expect(cli.executeOnText(code).errorCount).toBe(0);
+});
+
+test('key spacing', () => {
+    const code = `const obj = { foo: 42 };\n`;
     expect(cli.executeOnText(code).errorCount).toBe(0);
 });
