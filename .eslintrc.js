035053c166213f26d4591fb890c44b998a1769f3
mjeightyfive <mjeightyfive@gmail.com>
2020-03-21 11:38:56 +0000

    remove parser field

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
         navigator: false,
         window: false
     },
-    parser: 'babel-eslint',
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
